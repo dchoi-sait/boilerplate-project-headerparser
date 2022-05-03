1db6a071fab8cfe8d851d1a32e546aeb4d62b7ed
Daniel Choi <daniel.choi@edu.sait.ca>
2022-05-03 04:09:09 -0400

    header parser

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+// const { networkInterfaces } = require('os');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +23,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const header = { ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] }
+  res.json(header);
 });
 
 
